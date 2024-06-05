8723b7b89908f52844dcc2fb24a2527af8c2a4de
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2024-06-05 18:39:57 +0600

    React Router added

--- ChaatNChat-Frontend/chaatnchat/src/App.js
+++ ChaatNChat-Frontend/chaatnchat/src/App.js
@@ -2,15 +2,21 @@ import './App.css';
 import Login from './components/Auth/Login'
 import Register from './components/Auth/Register';
 import Chat from './components/Chat/Chat';
+import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Chaat N Chat</h1>
-      <Chat />
-      <Login />
-      <Register />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path='/' Component={Chat} />
+          <Route path='/login' Component={Login} />
+          <Route path='/register' Component={Register} />
+          <Route path='*' element={<h1>404 Page Not Found</h1>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
