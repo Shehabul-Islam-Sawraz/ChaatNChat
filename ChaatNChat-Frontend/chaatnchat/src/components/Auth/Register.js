5ca7e81cf1c2c7713cb690a29eb76a36ff91f663
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2024-06-11 02:01:45 +0600

    User registration functionalities done

--- ChaatNChat-Frontend/chaatnchat/src/components/Auth/Register.js
+++ ChaatNChat-Frontend/chaatnchat/src/components/Auth/Register.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import registerImage from '../../assets/images/register.svg'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 import './Auth.scss'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { register } from '../../store/actions/auth'
 
 const Register = () => {
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [gender, setGender] = useState('male')
+    const [password, setPassword] = useState('')
+
+    const submitForm = (e) => {
+        e.preventDefault()
+
+        dispatch(register({ firstName, lastName, email, gender, password }, navigate))
+    }
+
     return (
         <div id='auth-container'>
             <div id='auth-card'>
@@ -16,28 +35,52 @@ const Register = () => {
                     <div id='form-section'>
                         <h2>Create an account</h2>
 
-                        <form>
+                        <form onSubmit={submitForm}>
                             <div className='input-field mb-1'>
-                                <input placeholder='First Name' />
+                                <input
+                                    onChange={e => setFirstName(e.target.value)}
+                                    value={firstName}
+                                    required='required'
+                                    type='text'
+                                    placeholder='First Name' />
                             </div>
 
                             <div className='input-field mb-1'>
-                                <input placeholder='Last Name' />
+                                <input
+                                    onChange={e => setLastName(e.target.value)}
+                                    value={lastName}
+                                    required='required'
+                                    type='text'
+                                    placeholder='Last Name' />
                             </div>
 
                             <div className='input-field mb-1'>
-                                <input placeholder='Email' />
+                                <input
+                                    onChange={e => setEmail(e.target.value)}
+                                    value={email}
+                                    required='required'
+                                    type='text'
+                                    placeholder='Email' />
                             </div>
 
                             <div className='input-field mb-1'>
-                                <select>
+                                <select
+                                    onChange={e => setGender(e.target.value)}
+                                    value={gender}
+                                    required='required'
+                                >
                                     <option value='male'>Male</option>
                                     <option value='female'>Female</option>
                                 </select>
                             </div>
 
                             <div className='input-field mb-2'>
-                                <input placeholder='Password' />
+                                <input
+                                    onChange={e => setPassword(e.target.value)}
+                                    value={password}
+                                    required='required'
+                                    type='password'
+                                    placeholder='Password' />
                             </div>
 
                             <button>REGISTER</button>
--- ChaatNChat-Frontend/chaatnchat/src/store/actions/auth.js
+++ ChaatNChat-Frontend/chaatnchat/src/store/actions/auth.js
@@ -1,12 +1,25 @@
 import AuthService from "../../services/authService"
 
 export const LOGIN = 'LOGIN'
+export const REGISTER = 'REGISTER'
 
 export const login = (data, navigate) => dispatch => {
     return AuthService.login(data)
         .then(res => {
             console.log(res)
             dispatch({ type: LOGIN, payload: res })
+            navigate('/')
+        })
+        .catch(err => {
+
+        })
+}
+
+export const register = (data, navigate) => dispatch => {
+    return AuthService.register(data)
+        .then(res => {
+            console.log(res)
+            dispatch({ type: REGISTER, payload: res })
             navigate('/')
         })
         .catch(err => {
