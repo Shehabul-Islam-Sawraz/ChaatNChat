923f90fafa8ddb41ebfb89d7ed48508d69c47ce1
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2024-06-16 20:26:57 +0600

    Profile Dropdown added

--- ChaatNChat-Frontend/chaatnchat/src/components/Chat/components/Navbar/Navbar.js
+++ ChaatNChat-Frontend/chaatnchat/src/components/Chat/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.scss'
 import { useSelector } from 'react-redux'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -7,13 +7,23 @@ const Navbar = () => {
 
     const user = useSelector(state => state.authReducer.user)
 
+    const [showProfileOptions, setShowProfileOptions] = useState(false)
+
     return (
         <div id='navbar' className='card-shadow'>
             <h2>ChaatNChat</h2>
-            <div id='profile-menu'>
+            <div id='profile-menu' onClick={() => setShowProfileOptions(!showProfileOptions)}>
                 <img width='40' height='40' src={user.avatar} alt='Avatar' />
                 <p>{user.firstName} {user.lastName}</p>
                 <FontAwesomeIcon icon='caret-down' className='fa-icon' />
+
+                {
+                    showProfileOptions &&
+                    <div id='profile-options'>
+                        <p>Update Profile</p>
+                        <p>Log Out</p>
+                    </div>
+                }
             </div>
         </div>
     )
