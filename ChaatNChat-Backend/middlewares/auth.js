2a0c77f324a48ec0baf611e3df3c60d88b211b20
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2024-06-17 02:16:58 +0600

    Auth middleware created for update user information

---
+++ ChaatNChat-Backend/middlewares/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken')
+const config = require('../config/app')
+
+exports.auth = (req, res, next) => {
+
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1] // First element is bearer and second element is token
+
+    if (!token) {
+        return res.status(401).json({ error: 'Authentication Token Missing!' })
+    }
+
+    jwt.verify(token, config.appKey, (err, user) => {
+        if (err) {
+            return res.status(401).json({ err })
+        }
+
+        req.user = user
+        // console.log(user)
+    })
+
+    console.log(token)
+    next()
+}
