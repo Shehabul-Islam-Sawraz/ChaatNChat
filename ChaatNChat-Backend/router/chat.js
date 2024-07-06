2dacd191ee2a665c8c36760dce75a69abc21315a
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2024-07-06 18:20:09 +0600

    Deleting chat functionalities added

--- ChaatNChat-Backend/router/chat.js
+++ ChaatNChat-Backend/router/chat.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
-const { index, create, messages } = require('../controllers/chatController')
+const { index, create, messages, deleteChat } = require('../controllers/chatController')
 const { validate } = require('../validators')
 const { auth } = require('../middlewares/auth')
 
 router.get('/', [auth], index)
 router.post('/create', [auth], create)
 router.get('/messages', [auth], messages)
+router.delete('/:id', [auth], deleteChat)
 
 module.exports = router
