02667c971a8f16eddac1d47f98c5fddefc7279b7
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2024-07-06 17:28:26 +0600

    Chat messages pagination done

--- ChaatNChat-Backend/router/chat.js
+++ ChaatNChat-Backend/router/chat.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
-const { index, create } = require('../controllers/chatController')
+const { index, create, messages } = require('../controllers/chatController')
 const { validate } = require('../validators')
 const { auth } = require('../middlewares/auth')
 
 router.get('/', [auth], index)
 router.post('/create', [auth], create)
+router.get('/messages', [auth], messages)
 
 module.exports = router
