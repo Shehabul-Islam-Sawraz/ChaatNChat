b1465f31d2b174b1dac07fb28e6e60e0ddfa74bc
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2024-08-07 17:25:02 +0600

    Chat Header completed and Options of messeger added

--- ChaatNChat-Frontend/chaatnchat/src/components/Chat/components/ChatHeader/ChatHeader.js
+++ ChaatNChat-Frontend/chaatnchat/src/components/Chat/components/ChatHeader/ChatHeader.js
@@ -1,8 +1,70 @@
-const ChatHeader = () => {
+import React, { useState, Fragment } from 'react';
+import { userStatus } from '../../../../utils/helpers';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './ChatHeader.scss';
+
+const ChatHeader = ({ chat }) => {
+
+    const [showChatOptions, setShowChatOptions] = useState(false)
+    const [showAddFriendModal, setShowAddFriendModal] = useState(false)
+    const [showLeaveChatModal, setShowLeaveChatModal] = useState(false)
+    const [showDeleteChatModal, setShowDeleteChatModal] = useState(false)
+
     return (
-        <div>
-            <h1>Chat Header</h1>
-        </div>
+        <Fragment>
+            <div id='chatter'>
+                {
+                    chat.Users.map(user => {
+                        return (
+                            <div className='chatter-info'>
+                                <h3>{user.firstName} {user.lastName}</h3>
+                                <div className='chatter-status'>
+                                    <span className={`online-status ${userStatus(user)}`}></span>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <FontAwesomeIcon
+                onClick={() => setShowChatOptions(!showChatOptions)}
+                icon={['fas', 'ellipsis-v']}
+                className='fa-icon'
+            />
+            {
+                showChatOptions
+                    ? <div id='settings'>
+                        <div>
+                            <FontAwesomeIcon
+                                icon={['fas', 'user-plus']}
+                                className='fa-icon'
+                            />
+                            <p>Add a Friend</p>
+                        </div>
+
+                        {
+                            chat.type === 'group'
+                                ? <div>
+                                    <FontAwesomeIcon
+                                        icon={['fas', 'sign-out-alt']}
+                                        className='fa-icon'
+                                    />
+                                    <p>Leave Chat</p>
+                                </div>
+                                : null
+                        }
+
+                        <div>
+                            <FontAwesomeIcon
+                                icon={['fas', 'trash']}
+                                className='fa-icon'
+                            />
+                            <p>Delete Chat</p>
+                        </div>
+                    </div>
+                    : null
+            }
+        </Fragment>
     )
 }
 
